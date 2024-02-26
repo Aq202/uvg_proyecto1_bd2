c43e8384f106669bade3a8a8b0a9a403afb6e2fe
pabloozamora <pabloozamora02@gmail.com>
2024-02-26 17:40:10 -0600

    feat: Vista Lugares

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -5,6 +5,7 @@ import toggleNavBar from '../../hooks/toggleNavBar';
 import BottomWave from '../../components/BottomWave';
 import UserTrips from '../../components/UserTrips';
 import FindTrips from '../../components/FindTrips';
+import Places from '../../components/Places/Places';
 
 function MainPage() {
   const [currentSection, switchSection] = toggleNavBar();
@@ -20,6 +21,8 @@ function MainPage() {
       <div className={styles.pageContent}>
         {currentSection === 'userTrips' && <UserTrips />}
         {currentSection === 'findTrips' && <FindTrips />}
+        {currentSection === 'places' && <Places switchSection={switchSection} />}
+        {currentSection === 'editPlace' && <h1>Editar Lugar</h1>}
       </div>
     </div>
   );
