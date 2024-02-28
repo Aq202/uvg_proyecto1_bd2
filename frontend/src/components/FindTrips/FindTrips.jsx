5f5de12da5770a4af6cc4e0968e66d59e1ea1f21
pabloozamora <pabloozamora02@gmail.com>
2024-02-28 05:35:57 -0600

    feat: Asignarse a viajes

--- frontend/src/components/FindTrips/FindTrips.jsx
+++ frontend/src/components/FindTrips/FindTrips.jsx
@@ -1,17 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Pagination } from '@mui/material';
 import styles from './FindTrips.module.css';
 import InputSelect from '../InputSelect';
 import Trip from '../Trip';
+import useFetch from '../../hooks/useFetch';
+import { serverHost } from '../../config';
 
 function FindTrips() {
-  const [filters, setFilters] = useState();
+  const [filters, setFilters] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
-  const [trips, setTrips] = useState([
-    {
-      location: 'Hola', originName: 'Hola', originAddress: 'HOla', destinationName: 'Hola', destinationAddress: 'Hola', driver: 'Hola', passengers: 3, time: 'asdf',
-    },
-  ]);
+  const { callFetch: getRides, result: resultGet, error: errorGet } = useFetch();
+  const [trips, setTrips] = useState([]);
+
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZGRhNDM0MWIzMzk4YTBmODBjMWJjMyIsIm5hbWUiOiJQYWJsbyIsImVtYWlsIjoicGFibG9AZ21haWwuY29tIiwicGhvbmUiOiI1NTAwNDIzMyIsImlhdCI6MTcwOTAyNDMxOX0.Rql9zFZrTvBBgzTYxk56WFPpNUqLFEkXRUYOwXEt8Zs';
+
+  const getTrips = () => {
+    const { country, city, driver } = filters;
+    const paramsObj = { passenger: false };
+
+    if (country !== undefined && country !== '') {
+      paramsObj.country = country;
+    }
+
+    if (city !== undefined && city !== '') {
+      paramsObj.city = city;
+    }
+
+    if (driver !== undefined && driver !== '') {
+      paramsObj.driver = driver;
+    }
+
+    const searchParams = new URLSearchParams(paramsObj);
+    getRides({
+      uri: `${serverHost}/ride?${searchParams.toString()}`,
+      headers: { authorization: token },
+    });
+  };
+
+  const refreshTrips = () => {
+    setTrips([]);
+    setFilters({});
+    getTrips();
+  };
 
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
@@ -22,6 +52,43 @@ function FindTrips() {
     setTrips([]);
     setCurrentPage(page - 1);
   };
+
+  const readDate = (fechaISO) => {
+    const fecha = new Date(fechaISO);
+
+    // Convertir a cadena con formato local usando toLocaleString para manejar la zona horaria
+    const opciones = {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true,
+      timeZone: 'UTC', // Asegura que la conversión se haga respecto a la hora UTC
+    };
+
+    // Puedes cambiar 'es' por el código de tu zona horaria local si necesitas otro idioma o formato
+    const fechaHoraFormateada = fecha.toLocaleString('es', opciones);
+
+    // Para asegurar el formato deseado, podrías hacer un ajuste manual si es necesario
+    // Esto es un ejemplo y podría necesitar ajustes dependiendo del idioma o la zona horaria
+    return fechaHoraFormateada.replace(/\//g, '-').replace(',', '');
+  };
+
+  useEffect(() => {
+    if (resultGet) {
+      setTrips(resultGet.result);
+    }
+  }, [resultGet]);
+
+  useEffect(() => {
+    setTrips([]);
+    getTrips();
+  }, [currentPage, filters]);
+
+  useEffect(() => {
+    getTrips();
+  }, []);
 
   return (
     <div className={styles.mainContainer}>
@@ -64,20 +131,27 @@ function FindTrips() {
         </div>
       </div>
 
-      <div className={styles.tripsContainer}>
-        {trips.map((trip) => (
-          <Trip
-            location={trip.location}
-            originName={trip.originName}
-            originAddress={trip.originAddress}
-            destinationName={trip.destinationName}
-            destinationAddress={trip.destinationAddress}
-            driver={trip.driver}
-            passengers={trip.passengers}
-            time={trip.time}
-          />
-        ))}
-      </div>
+      {!errorGet && (
+        <div className={styles.tripsContainer}>
+          {trips.map((trip) => (
+            <Trip
+              id={trip.id}
+              location={`${trip.arrivalLocation.city}, ${trip.arrivalLocation.country}`}
+              originName={trip.startLocation.name}
+              originAddress={trip.startLocation.address}
+              destinationName={trip.arrivalLocation.name}
+              destinationAddress={trip.arrivalLocation.address}
+              driver={trip.user.name}
+              passengers={trip.passengers.length}
+              time={readDate(trip.datetime)}
+              joined={trip.isPassenger}
+              callback={refreshTrips}
+            />
+          ))}
+        </div>
+      )}
+
+      {errorGet && <p>No se encontraron resultados</p>}
 
       <Pagination
         count={0}
--- frontend/src/components/UserTrips/UserTrips.jsx
+++ frontend/src/components/UserTrips/UserTrips.jsx
@@ -1,17 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Pagination } from '@mui/material';
 import styles from './UserTrips.module.css';
 import InputSelect from '../InputSelect';
 import Trip from '../Trip';
+import useFetch from '../../hooks/useFetch';
+import { serverHost } from '../../config';
 
 function UserTrips() {
-  const [filters, setFilters] = useState();
+  const [filters, setFilters] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
-  const [trips, setTrips] = useState([
-    {
-      location: 'Hola', originName: 'Hola', originAddress: 'HOla', destinationName: 'Hola', destinationAddress: 'Hola', driver: 'Hola', passengers: 3, time: 'asdf',
-    },
-  ]);
+  const [trips, setTrips] = useState([]);
+  const { callFetch: getRides, result: resultGet, error: errorGet } = useFetch();
+
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZGRhNDM0MWIzMzk4YTBmODBjMWJjMyIsIm5hbWUiOiJQYWJsbyIsImVtYWlsIjoicGFibG9AZ21haWwuY29tIiwicGhvbmUiOiI1NTAwNDIzMyIsImlhdCI6MTcwOTAyNDMxOX0.Rql9zFZrTvBBgzTYxk56WFPpNUqLFEkXRUYOwXEt8Zs';
+
+  const getUserTrips = () => {
+    const { country, city, driver } = filters;
+    const paramsObj = { passenger: true };
+
+    if (country !== undefined && country !== '') {
+      paramsObj.country = country;
+    }
+
+    if (city !== undefined && city !== '') {
+      paramsObj.city = city;
+    }
+
+    if (driver !== undefined && driver !== '') {
+      paramsObj.driver = driver;
+    }
+
+    const searchParams = new URLSearchParams(paramsObj);
+    getRides({
+      uri: `${serverHost}/ride?${searchParams.toString()}`,
+      headers: { authorization: token },
+    });
+  };
+
+  const refreshTrips = () => {
+    setTrips([]);
+    setFilters({});
+    getUserTrips();
+  };
 
   const handleFilterChange = (e) => {
     const { name, value } = e.target;
@@ -22,6 +52,21 @@ function UserTrips() {
     setTrips([]);
     setCurrentPage(page - 1);
   };
+
+  useEffect(() => {
+    if (resultGet) {
+      setTrips(resultGet.result);
+    }
+  }, [resultGet]);
+
+  useEffect(() => {
+    setTrips([]);
+    getUserTrips();
+  }, [currentPage, filters]);
+
+  useEffect(() => {
+    getUserTrips();
+  }, []);
 
   return (
     <div className={styles.mainContainer}>
@@ -64,20 +109,27 @@ function UserTrips() {
         </div>
       </div>
 
-      <div className={styles.tripsContainer}>
-        {trips.map((trip) => (
-          <Trip
-            location={trip.location}
-            originName={trip.originName}
-            originAddress={trip.originAddress}
-            destinationName={trip.destinationName}
-            destinationAddress={trip.destinationAddress}
-            driver={trip.driver}
-            passengers={trip.passengers}
-            time={trip.time}
-          />
-        ))}
-      </div>
+      {!errorGet && (
+        <div className={styles.tripsContainer}>
+          {trips.map((trip) => (
+            <Trip
+              id={trip.id}
+              location={`${trip.arrivalLocation.city}, ${trip.arrivalLocation.country}`}
+              originName={trip.startLocation.name}
+              originAddress={trip.startLocation.address}
+              destinationName={trip.arrivalLocation.name}
+              destinationAddress={trip.arrivalLocation.address}
+              driver={trip.user.name}
+              passengers={trip.passengers.length}
+              time={trip.datetime}
+              joined={trip.isPassenger}
+              callback={refreshTrips}
+            />
+          ))}
+        </div>
+      )}
+
+      {errorGet && <p>No se encontraron resultados</p>}
 
       <Pagination
         count={0}
