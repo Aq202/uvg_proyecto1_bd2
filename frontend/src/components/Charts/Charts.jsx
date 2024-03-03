17416ade762345334c4668a74ff44745b2453e24
Erick Guerra <erickguerra2201@gmail.com>
2024-03-03 10:48:38 -0600

    Top ridres

--- frontend/src/components/Charts/Charts.jsx
+++ frontend/src/components/Charts/Charts.jsx
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Charts.module.css';
+import Table from '../Table';
+import TableRow from '../TableRow';
+import useFetch from '../../hooks/useFetch';
+import { serverHost } from '../../config';
+import useToken from '../../hooks/useToken';
+import Spinner from '../Spinner';
 
 function Charts() {
+  const [top, setTop] = useState([]);
+  const token = useToken();
+  const {
+    callFetch: getTop,
+    result: resultGet,
+    error: errorGet,
+    loading: loadingGet,
+  } = useFetch();
+
+  const getUserTop = () => {
+    getTop({
+      uri: `${serverHost}/ride/user/top`,
+      headers: { authorization: token },
+    });
+  };
+
+  useEffect(() => {
+    if (resultGet) {
+      setTop(resultGet);
+    }
+  }, [resultGet]);
+
+  useEffect(() => {
+    getUserTop();
+  }, []);
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.headerSection}>
         <p className={styles.title}>Dashboard</p>
       </div>
+      {loadingGet && <Spinner />}
+      {errorGet && <p>{`Ha ocurrido un error ${errorGet}`}</p>}
+      {!errorGet && !loadingGet && top && top.length > 0 && (
+        <>
+          <div className={styles.headerSection}>
+            <p className={styles.subTitle}>Top usuarios con más viajes completados</p>
+          </div>
+          <Table minCellWidth="50px" breakPoint="700px" showCheckbox={false} header={['Usuario', 'Viajes completados']}>
+            {top.map((row) => (
+              <TableRow key={row.user.id} id={row.user.id}>
+                <td
+                  className={styles.tableCell}
+                >
+                  {row.user.name}
+                </td>
+                <td
+                  className={styles.tableCell}
+                >
+                  {row.totalTrips}
+                </td>
+              </TableRow>
+            ))}
+          </Table>
+        </>
+      )}
       <iframe
         title="dashboard"
         style={{
